c8474302d60a965551369aa9c7d62e3712b33005
张灿zyxwz <zhangcan2000@gmail.com>
2026-07-04 16:42:56 +0800

    Update 1_👤_单户评估.py

--- Credit_Risk_Assessment_Model/system/pages/1_👤_单户评估.py
+++ Credit_Risk_Assessment_Model/system/pages/1_👤_单户评估.py
@@ -27,7 +27,7 @@ st.set_page_config(
 )
 
 from core.predictor import predict, get_model
-from core.config import (
+from system.core.config import (
     SYSTEM_NAME, ORGANIZATION, RESPONSIBLE,
     HISTORY_DIR, CROP_SUBSIDY_RATE, CROP_REVENUE_RATE
 )
